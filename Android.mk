c7718093c7b481e95d75f974f4fcecc54c7916a2
kmiit <kmiit@foxmail.com>
2025-03-21 21:03:29 +0800

    init

---
+++ Android.mk
@@ -0,0 +1,35 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),shennong)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,82ddbddd4d70d41f12e2b8c96ab785ba3d810885)
+$(call add-radio-file-sha1-checked,radio/aop.img,927ed2bb188678cd3570ff7488d5f4a89ed348f5)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,091ddb8e33d40624391b3fba614b24f309ec2b52)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,64c63f9c8fab1a142eede169efefea3875457610)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,08d52985921a34cb3114cff59b9b0ca7b64da428)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,3536912494603134cf955fdbe62848e6314f91b0)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,6d50b0ad3a0d580a4d55b811aed29c2fa2cceadf)
+$(call add-radio-file-sha1-checked,radio/dsp.img,e623b4401f2fada542ef7274b42c0fb0f3e3a91d)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,81fc291614567712e56745475ba41c7482f69143)
+$(call add-radio-file-sha1-checked,radio/hyp.img,db0ee3294d0f7fbbc30909a18114579837e46d68)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,91272db3c4eea5e952f8298635b2433535b877f2)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,7d70b4af7e7b57a6b21b1dfb2a98220f6fc915e2)
+$(call add-radio-file-sha1-checked,radio/modem.img,33572f47003e699086a27550f44b898d43986a6c)
+$(call add-radio-file-sha1-checked,radio/modemfirmware.img,69724197e5ab1a978cc34f2e2a3c003a164abf07)
+$(call add-radio-file-sha1-checked,radio/multiimgqti.img,2ba8087f06f20e319b9e7570879940555f8f872b)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,889b16752e4a729ae9ce9335ab8877b732d37131)
+$(call add-radio-file-sha1-checked,radio/shrm.img,fa1a8b568e839fe3e4997ddc78e96421535ded3b)
+$(call add-radio-file-sha1-checked,radio/spuservice.img,67989c90e420aa1c1776a116b9b94442a44d721e)
+$(call add-radio-file-sha1-checked,radio/tz.img,2d439671bce3244da3f99192158580885246b476)
+$(call add-radio-file-sha1-checked,radio/uefi.img,e17fa808ef780dad22d628ee86991e44fd3e7890)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,909ab0eb6bbc9c47937a04cdc5c387533a37dcb5)
+$(call add-radio-file-sha1-checked,radio/vm-bootsys.img,0ddc2f150b5a97035d9dfd363ebd555e4813d898)
+$(call add-radio-file-sha1-checked,radio/xbl.img,4b6db29497436e6d5c60db8d8dbc0a93f93ac755)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,264b24851cca25fb97e35a3440cd571e14e18204)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,fddb57c0d91de46320d12205532231f8beffe1f6)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    cpucp_dtb \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    modemfirmware \
+    multiimgqti \
+    qupfw \
+    shrm \
+    spuservice \
+    tz \
+    uefi \
+    uefisecapp \
+    vm-bootsys \
+    xbl \
+    xbl_config \
+    xbl_ramdump
